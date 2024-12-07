f198ca77cc7f27dcec0f635c399832023b9c3c13
dipen.patel <dipen.patel@covrtech.com>
2024-12-07 14:12:38 +0530

    Initialize Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Router and Routes
+import { ThemeProvider } from '@mui/material';
+import { theme } from "./theme";
+import CompanyList from "./components/modules/company/CompanyList";
+import AddCompanyForm from "./components/modules/company/AddCompanyForm";
+import CompanyPage from "./components/modules/items/ItemsList";
+import Invoice from "./components/modules/invoice/Invoice";
+import Layout from "./components/shared/Layout.js";
+import InvoiceDetails from "./components/modules/invoice/InvoiceDetails.js";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<CompanyList />} />
+            <Route path="/add-company" element={<AddCompanyForm />} />
+            <Route path="/company" element={<CompanyPage />} />
+            <Route path="/invoice" element={<Invoice />} />
+            <Route path="/invoice/your-invoice/:id" element={<InvoiceDetails />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   );
 }
 
